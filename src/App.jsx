073a070f61813ce71700bcdac7698f302d2b6c65
tuhar <tusharhirawat1918@gmail.com>
2025-02-21 17:12:55 +0530

    no chnges made

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,4 @@
 
-// import React from "react";
-// import styled from "styled-components";
-// import Login from "./components/Login";
-// import Logout from "./components/Logout";
-// import { useMsal } from "@azure/msal-react";
-// import SecureData from "./SecureData";
-
-import AppRouter from "./Routes/AppRouter";
-
-// const Container = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   height: 100vh;
-//   background-color: #121212;
-//   color: #fff;
-//   font-family: Arial, sans-serif;
-// `;
-
-// const Heading = styled.h1`
-//   font-size: 2rem;
-//   margin-bottom: 20px;
-// `;
-
-// const UserInfo = styled.div`
-//   text-align: center;
-//   margin-top: 20px;
-//   padding: 20px;
-//   background: #1e1e1e;
-//   border-radius: 10px;
-//   box-shadow: 0px 4px 10px rgba(255, 255, 255, 0.1);
-// `;
-
-// const App = () => {
-//   const { accounts } = useMsal();
-//   console.log(accounts);
-
-//   return (
-//     <Container>
-//       <Heading>Azure AD B2C Authentication</Heading>
-//       {accounts.length > 0 ? (
-//         <UserInfo>
-//           <h2>Welcome, {accounts[0].name}</h2>
-//           {/* <h4>{accounts[0].username}</h4> */}
-//           <h2>{accounts[0].idTokenClaims.family_name}</h2>
-//           <h2>{accounts[0].idTokenClaims.given_name}</h2>
-//           <Logout />
-//           {/* <SecureData /> */}
-//         </UserInfo>
-//       ) : (
-//         <Login />
-//       )}
-//     </Container>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
 
 // import React, { useState, useEffect } from "react";
 // import styled from "styled-components";
@@ -175,7 +107,8 @@ import AppRouter from "./Routes/AppRouter";
 
 
 
-
+import AppRouter from "./Routes/AppRouter";
+import React from "react";
 
 const App = () => {
   return (<>
--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,7 @@ import App from "./App";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
+import "./index.css";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
