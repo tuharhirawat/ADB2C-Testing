675f68fc42366c0106758cdb2696cbe60a4cee36
tuhar <tusharhirawat1918@gmail.com>
2025-02-25 15:50:51 +0530

    no changes made

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,20 +1,3 @@
-// import React, { Children } from "react";
-// import Header from "../Components/Header";
-// import Footer from "../Components/Footer";
-
-// function MainLayout({ children }) {
-//   return (
-//     <div>
-//       <Header />
-//       <main>{children}</main>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default MainLayout;
-
-
 
 import React from "react";
 import Header from "../Components/Header";
